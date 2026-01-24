dba3ec73a05d3f38ebf1170af544393729f8f13f
jianliang00 <wangjianliang0@126.com>
2026-01-24 20:42:22 +0800

    Initial Commit

---
+++ OpenBoxTests/OpenBoxTests.swift
@@ -0,0 +1,16 @@
+//
+//  OpenBoxTests.swift
+//  OpenBoxTests
+//
+//  Created by jianliang on 2026/1/24.
+//
+
+import Testing
+
+struct OpenBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
